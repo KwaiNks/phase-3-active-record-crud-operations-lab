2fcfd897c5c52a47c233a92c62ea65c1f64c3faf
Richmond Nkansah <richmondnkansah@gmail.com>
2022-11-29 21:19:59 -0500

    commit tree c8c2c63393a7338a3bdb7a216c058b2224e0c3be

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,51 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+       Movie.create(title: title)
+    end
+     #//class methods (.)
+    def self.first_movie
+        Movie.first
+    end
+     #//class methods (.)
+    def self.last_movie
+        Movie.last
+    end
+     #//class methods (.)
+    def self.movie_count
+        Movie.count
+    end
+     #//class methods (.)
+    def self.find_movie_with_id(id)
+        Movie.find(id)   
+    end
+      #//class methods (.)
+    def self.find_movie_with_attributes(attribute)
+        Movie.find_by(attribute)
+    end
+      #//class methods (.)
+    def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+    end
+
+    #//instance methods (#) 
+    def update_with_attributes(attribute)
+      self.update(attribute)
+    end
+
+    #//class methods (.)((
+    def self.update_all_titles(title)
+        Movie.update(title: title)
+    end
+
+     #//instance methods (#) 
+    def self.delete_by_id(id)
+        movie = Movie.find(id)
+        movie.destroy
+    end
+
+    def self.delete_all_movies
+     Movie.destroy_all
+    end
+
 end
